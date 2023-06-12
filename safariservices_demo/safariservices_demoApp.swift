a0ffd8addc3e2708f2de8e5bff372a8161c8e5d5
Edward Yee <yeeedward@hotmail.com>
2023-06-12 12:29:46 +0800

    Initial Commit

---
+++ safariservices_demo/safariservices_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  safariservices_demoApp.swift
+//  safariservices_demo
+//
+//  Created by Edward Yee on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct safariservices_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
